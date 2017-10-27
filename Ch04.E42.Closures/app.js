9d0daf50042368e4a8345e5fd13109ca811f36a2
oyinkan <oyink7@gmail.com>
2017-10-27 20:01:45 +0100

    closures part 2

--- Ch04.E42.Closures/app.js
+++ Ch04.E42.Closures/app.js
@@ -6,9 +6,35 @@ function greet(whattosay) {
 
     return function(name) {
         console.log(whattosay + ' ' + name);
-    }
+        console.log('end of part 1-------------');
+    };
 
 }
 
 var sayHi = greet('Hi');
 sayHi('Tony');
+
+//Part 2 
+
+function buildFunctions() {
+
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j);
+                }
+            })(i));
+    }
+
+    return arr;
+}
+
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
+
